2f3bcb7fc0c2f72963f89bf3bdaf0b45a4d3c18f
Ty Streets <tstreets@iu.edu>
2024-11-29 07:19:01 -0500

    There ya go

--- api/index.js
+++ api/index.js
@@ -1,5 +1,24 @@
-const router = require("express").Router();
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
 
-router.use("/fall-24", require("./fall-24"));
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-module.exports = router;
+app.use(express.static(path.join(__dirname, "views")));
+
+const port = process.env.PORT || 3008;
+process.env.BASE_URL =
+  process.env.NODE_ENV === "DEV"
+    ? `http://localhost:${port}`
+    : process.env.BASE_URL;
+
+app.use("/api/fall-24", require("./fall-24"));
+
+app.listen(port);
+
+if (process.env.NODE_ENV === "DEV")
+  console.log(`Server running: http://localhost:${port}`);
+
+module.exports = app;
