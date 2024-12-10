0940d9ac36e2a68b00187ca78b77cfe54213ce69
Ty Streets <tstreets@iu.edu>
2024-12-10 10:29:33 -0500

    Added pennywise

--- api/fall-24/underrated-music/data.js
+++ api/fall-24/underrated-music/data.js
@@ -18,7 +18,6 @@ const data = [
       `${process.env.BASE_URL}/resources/underrated-music/songs/Cheap Trick - Surrender (Official Audio).mp3`,
     ],
   },
-
   {
     name: "Pennywise",
     url: "https://pennywisdom.com/",
@@ -27,7 +26,37 @@ const data = [
       `${process.env.BASE_URL}/resources/underrated-music/songs/Pennywise - Bro Hymn (Full Album Stream).mp3`,
       `${process.env.BASE_URL}/resources/underrated-music/songs/Pennywise - Same Old Story.mp3`,
       `${process.env.BASE_URL}/resources/underrated-music/songs/PennyWise-Fuck Authority.mp3`,
+    ],
+  },
+  {
+    name: "Queens of the Stone Age",
+    url: "https://qotsa.com/",
+    cover_image: `${process.env.BASE_URL}/resources/underrated-music/album-covers/QueensoftheStoneAgeI.jpg`,
+    songs: [
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Queens of the Stone Age - I Sat By The Ocean (Official Audio).mp3`,
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Queens Of The Stone Age - Little Sister.mp3`,
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Queens of the Stone Age - My God Is The Sun.mp3`,
       // `${process.env.BASE_URL}/resources/underrated-music/songs/replace`,
+    ],
+  },
+  {
+    name: "Three Days Grace",
+    url: "https://threedaysgrace.com/",
+    cover_image: `${process.env.BASE_URL}/resources/underrated-music/album-covers/One_X.png`,
+    songs: [
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Three Days Grace - Animal I Have Become.mp3`,
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Three Days Grace - Pain.mp3`,
+      `${process.env.BASE_URL}/resources/underrated-music/songs/On My Own.mp3`,
+    ],
+  },
+  {
+    name: "Pixies",
+    url: "https://www.pixiesmusic.com/",
+    cover_image: `${process.env.BASE_URL}/resources/underrated-music/album-covers/P.jpg`,
+    songs: [
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Pixies - Gigantic [HD].mp3`,
+      `${process.env.BASE_URL}/resources/underrated-music/songs/Where Is My Mind_ (2007 Remaster).mp3`,
+      `${process.env.BASE_URL}/resources/underrated-music/songs/The Holiday Song.mp3`,
     ],
   },
 ];
