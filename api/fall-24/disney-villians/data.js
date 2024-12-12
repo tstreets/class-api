2907b7d11f3e0d71e7d72a6351e816efe862f2a7
Ty Streets <tstreets@iu.edu>
2024-12-11 19:45:40 -0500

    Added disney villians api

---
+++ api/fall-24/disney-villians/data.js
@@ -0,0 +1,147 @@
+const data = [
+  {
+    name: "The Evil Queen",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/8/89/Profile_-_The_Evil_Queen.jpeg/revision/latest?cb=20190312020700",
+    films: [
+      "Snow White and the Seven Dwarfs",
+      "Who Framed Roger Rabbit (cameo)",
+      "Golden Anniversary of Snow White and the Seven Dwarfs",
+      "The Nightmare Before Christmas (cameo)",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+      "Pinocchio (2022) (cameo)",
+    ],
+    year: "1937",
+  },
+  {
+    name: "Chernabog",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/6/61/Profile_-_Chernabog.jpeg/revision/latest?cb=20190312041146",
+    films: [
+      "Fantasia",
+      "The Reluctant Dragon (cameo)",
+      "Fantasia 2000 (appeared in a flashback)",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+      "The Sorcerer's Apprentice (as a statue)",
+    ],
+    year: "1940",
+  },
+  {
+    name: "Queen of Hearts",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/6/61/Profile_-_Queen_of_Hearts.jpeg/revision/latest?cb=20190312053228",
+    films: [
+      "Alice in Wonderland",
+      "Mickey's House of Villains",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Once Upon a Halloween",
+    ],
+    year: "1951",
+  },
+  {
+    name: "Captain Hook",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/0/0b/Profile_-_Captain_Hook.jpeg/revision/latest/scale-to-width-down/1000?cb=20190312022618",
+    films: [
+      "Peter Pan",
+      "Return to Never Land",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+    ],
+    year: "1953",
+  },
+  {
+    name: "Maleficent",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/0/05/Profile_-_Maleficent.jpeg/revision/latest?cb=20190312020410",
+    films: [
+      "Sleeping Beauty",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+      "Pinocchio (2022) (cameo)",
+    ],
+    year: "1959",
+  },
+  {
+    name: "Cruella De Vil",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/9/92/Profile_-_Cruella.png/revision/latest?cb=20190312122644",
+    films: [
+      "One Hundred and One Dalmatians",
+      "Mickey's House of Villains",
+      "101 Dalmatians II: Patch's London Adventure",
+      "Once Upon a Halloween",
+    ],
+    year: "1961",
+  },
+  {
+    name: "Ursula",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/7/72/Profile_-_Ursula.jpeg/revision/latest/scale-to-width-down/1000?cb=20201204230949",
+    films: [
+      "The Little Mermaid",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+    ],
+    year: "1989",
+  },
+  {
+    name: "Jafar",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/6/60/Profile_-_Jafar.jpeg/revision/latest/scale-to-width-down/1000?cb=20190312023726",
+    films: [
+      "Aladdin",
+      "The Return of Jafar",
+      "Aladdin and the King of Thieves (cameo)",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+    ],
+    year: "1992",
+  },
+  {
+    name: "Scar",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/6/66/Profile_-_Scar.jpeg/revision/latest/scale-to-width-down/1000?cb=20190312021344",
+    films: [
+      "The Lion King",
+      "The Lion King II: Simba's Pride",
+      "The Lion King 1½",
+      "Hercules (cameo)",
+      "Once Upon a Halloween",
+      "The Lion Guard: Return of the Roar (cameo)",
+    ],
+    year: "1994",
+  },
+  {
+    name: "Hades",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/2/2c/Profile_-_Hades.jpeg/revision/latest/scale-to-width-down/1000?cb=20190312030411",
+    films: [
+      "Hercules",
+      "Hercules: Zero to Hero",
+      "Mickey's Magical Christmas: Snowed in at the House of Mouse",
+      "Mickey's House of Villains",
+      "Once Upon a Halloween",
+      "Descendants 3",
+      "Descendants: The Royal Wedding",
+      "Descendants: The Rise of Red",
+    ],
+    year: "1997",
+  },
+  {
+    name: "Dr. Facilier",
+    image:
+      "https://static.wikia.nocookie.net/disney/images/3/3f/Profile_-_Dr._Facilier.jpeg/revision/latest?cb=20190312024000",
+    films: ["The Princess and the Frog"],
+    year: "2009",
+  },
+];
+
+module.exports = data;
--- api/fall-24/index.js
+++ api/fall-24/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
 router.use("/underrated-music", require("./underrated-music"));
+router.use("/disney-villians", require("./disney-villians"));
 
 module.exports = router;
